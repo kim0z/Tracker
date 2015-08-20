a1fa4b58e55012d80a2ccc2ac7126225faee60ac
Karim Fanadka <kareem9k@gmail.com>
2015-08-21 00:21:03 +0300

    Layout change + dynamic input + number picker

--- public/view1/view1.js
+++ public/view1/view1.js
@@ -1,12 +1,14 @@
 'use strict';
 
-var app = angular.module('myApp.view1', ['ngRoute','uiGmapgoogle-maps'])
+var app = angular.module('myApp.view1', ['ngRoute','uiGmapgoogle-maps','angularNumberPicker'])
 
 .config(['$routeProvider','uiGmapGoogleMapApiProvider', function($routeProvider,GoogleMapApiProviders) {
   $routeProvider.when('/view1', {
     templateUrl: 'view1/view1.html',
     controller: 'View1Ctrl',
-	controller: 'ExampleController'
+	controller: 'ExampleController',
+	controller: 'NumberPicker',
+	controller: 'View1Ctrl'
 	
   });
    GoogleMapApiProviders.configure({
@@ -23,8 +25,8 @@ app.controller('View1Ctrl', function($scope) {
 		zoom: 13
 		};
 });
-	
-			
+
+//submit
 app.controller('ExampleController', ['$scope', '$http', function($scope, $http) {
 
     $scope.submit = function() {
@@ -46,17 +48,17 @@ app.controller('ExampleController', ['$scope', '$http', function($scope, $http) 
       };
  }]);
 			
-			
- app.controller('ExampleController', ['$scope','$http', function($scope, $http) {
-    $scope.example = {
-        text: 'guest',
+//cities and trip definition 			
+ app.controller('tripDefinitionCtrl', ['$scope','$http', function($scope, $http) {
+    $scope.tripName = {
+        text: 'Trip Name 1',
         word: /^\s*\w*\s*$/
     };
 
     $scope.onBlur = function($event) {
 
-    	var dataObj = {city: $scope.example.text};
-    	console.log($scope.example.text);
+    	var dataObj = {city: $scope.destination1.text};
+    	console.log($scope.destination1.text);
 
         		var res = $http.post('/getGeoCode', dataObj);
 				res.success(function(data, status, headers, config) {
@@ -73,3 +75,51 @@ app.controller('ExampleController', ['$scope', '$http', function($scope, $http) 
 
  }]);
 
+ //Number picker controller
+app.controller('NumberPicker', ['$scope', function($scope) {
+   $scope.input = {
+            num: 0
+        };
+
+   $scope.getNumber = function() {
+       alert('The number is: [' + $scope.input.num + ']');
+   };
+
+   $scope.onChange = function(){
+       console.log('The number is Changed ', $scope.input.num);
+   };
+}]);
+
+
+
+//Directive that returns an element which adds buttons on click which show an alert on click
+app.directive("addbuttonsbutton", function(){
+	return {
+		restrict: "E",
+		template: "<button addbuttons>Click to add buttons</button>"
+	}
+});
+
+//Directive for adding buttons on click that show an alert on click
+app.directive("addbuttons", function($compile){
+	
+	return function(scope, element, attrs){
+		element.bind("click", function(){
+			scope.count++;
+			angular.element(document.getElementById('space-for-input')).append($compile('<label>Destination 1:</label><input type="text" name="input" ng-model="destination1.text" ng-pattern="example.word" required ng-trim="false" ng-blur="onBlur($event)"')(scope));
+		});
+	};
+});
+
+//creates circles on the map
+app.controller('CircleSimpleCtrl', function($scope) {
+  $scope.cities = {
+    chicago: {population:2714856, position: [41.878113, -87.629798]},
+    newyork: {population:8405837, position: [40.714352, -74.005973]},
+    losangeles: {population:3857799, position: [34.052234, -118.243684]},
+    vancouver: {population:603502, position: [49.25, -123.1]},
+  }
+  $scope.getRadius = function(num) {
+    return Math.sqrt(num) * 100;
+  }
+});
