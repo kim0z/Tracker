0f7a815efb2dbe5bcfa06dcd9768d36c0e9e7fd8
Karim <karim@Karims-MacBook-Air.local>
2015-11-04 22:06:52 +0200

    homepage

--- public/views/view0/ctrl.js
+++ public/views/view0/ctrl.js
@@ -6,5 +6,36 @@ trackerApp.controller('view0Ctrl',function($scope, $http, googleMapsAPIService) 
     $scope.msg = 'clicked';
   }
 
+
+
+    var i = 0;
+
+    var videoSource = new Array();
+    videoSource[0]='../../assets/videos/IMG_0302.MOV';
+    videoSource[1]='../../assets/videos/IMG_0349.MOV';
+    videoSource[2]='../../assets/videos/IMG_0317.MOV';
+    var videoCount = videoSource.length;
+
+    document.getElementById('myVideo').addEventListener('ended',myHandler,false);
+    document.getElementById("myVideo").setAttribute("src",videoSource[0]);
+
+
+    function myHandler() {
+
+        i++;
+        if(i == videoCount)
+            i = 0;
+        videoPlay(i);
+
+    }
+
+    function videoPlay(videoNum)
+    {
+        document.getElementById("myVideo").setAttribute("src",videoSource[videoNum]);
+        document.getElementById("myVideo").load();
+        document.getElementById("myVideo").play();
+    }
+
+
   
 });
