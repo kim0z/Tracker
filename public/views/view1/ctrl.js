03478ba1c82b89c3905609b7c332a8a6b574cede
Karim <karim@Karims-MacBook-Air.local>
2015-11-07 15:57:22 +0200

    tabel structure

--- public/views/view1/ctrl.js
+++ public/views/view1/ctrl.js
@@ -1,27 +1,6 @@
 trackerApp.controller('view1Ctrl' ,function ($scope, $http, googleMapsAPIService, dataBaseService ,messages, NgTableParams) {
 
 
-
-    $scope.users = [{"id":1,"first_name":"Philip","last_name":"Kim","email":"pkim0@mediafire.com","country":"Indonesia","ip_address":"29.107.35.8"},
-        {"id":2,"first_name":"Judith","last_name":"Austin","email":"jaustin1@mapquest.com","country":"China","ip_address":"173.65.94.30"},
-        {"id":3,"first_name":"Julie","last_name":"Wells","email":"jwells2@illinois.edu","country":"Finland","ip_address":"9.100.80.145"},
-        {"id":4,"first_name":"Gloria","last_name":"Greene","email":"ggreene3@blogs.com","country":"Indonesia","ip_address":"69.115.85.157"},
-    {"id":50,"first_name":"Andrea","last_name":"Greene","email":"agreene4@fda.gov","country":"Russia","ip_address":"128.72.13.52"}];
-
-
-
-    $scope.usersTable = new NgTableParams({
-        page: 1,
-        count: 10
-    }, {
-        total: $scope.users.length,
-        getData: function ($defer, params) {
-            $scope.data = $scope.users.slice((params.page() - 1) * params.count(), params.page() * params.count());
-            $defer.resolve($scope.data);
-        }
-    });
-
-
     //get trip data to the page
     $scope.trip_id = messages.getTripID();
 
@@ -75,6 +54,24 @@ trackerApp.controller('view1Ctrl' ,function ($scope, $http, googleMapsAPIService
             var self = this;
             var data = [{name: "Moroni", age: 50},{name: "Moroni", age: 50}];
             self.tableParams = new NgTableParams({}, { dataset: data});
+
+
+
+
+            $scope.users = [{day:'1', city:'London'},{day:'2', city:'TLV'}];
+
+
+
+            $scope.usersTable = new NgTableParams({
+                page: 1,
+                count: 10
+            }, {
+                total: $scope.users.length,
+                getData: function ($defer, params) {
+                    $scope.data = $scope.users.slice((params.page() - 1) * params.count(), params.page() * params.count());
+                    $defer.resolve($scope.data);
+                }
+            });
 
 
             console.log('Client:: View3:: Create Table::' +  $scope.table);
