32416268225d60e6416adde5e9b7226cabbd5bb9
Karim <karim@Karims-MacBook-Air.local>
2016-03-12 23:01:01 +0200

    multi polyline

--- public/views/view2/ctrl.js
+++ public/views/view2/ctrl.js
@@ -4,128 +4,49 @@ trackerApp.controller('view2Ctrl', function ($scope, $firebaseObject, $http, $do
         $scope.travelersList = [];
         $scope.data = []; // Travellers from PG DB
         $scope.tips = []; // Tips from Firebase, based on GPS point
+        var users_hash = {};
 
         // Get a database reference to our posts
         var ref = new Firebase("https://luminous-torch-9364.firebaseio.com/");
 
         //$scope.init = function () {
-        var trackCoordinates = [];
+        var trackCoordinates = []; // for new GPS points
+
+
+        var coordinates = {
+            "feed1": [
+                {lat: 25.774252, lang: -80.190262},
+                {lat: 35.774252, lang: -80.190262}
+            ],
+
+            "feed2": [
+                {lat: 25.774252, lang: -80.190262},
+                {lat: 26.774252, lang: -80.190262}
+            ],
+
+            "feed3": [
+                {lat: 25.774252, lang: -50.190262},
+                {lat: 75.774252, lang: -80.190262}
+            ]
+        };
+
+
         $scope.map;
         $scope.lastGPSpoint = "";
 
+        //Map configuration
         $scope.map = new google.maps.Map(document.getElementById('map'), {
-            center: {lat: 34.397, lng: 40.644},
-            zoom: 5
+            //center: {lat: 34.397, lng: 40.644},
+            center: {lat: 0, lng: 0},
+            zoom: 5,
+            mapTypeId: google.maps.MapTypeId.TERRAIN
         });
 
-//////////////
-    /// Maps overlay
 
-/*
-        var overlay;
-        USGSOverlay.prototype = new google.maps.OverlayView();
+        /// Map configuration END
 
 
-        var bounds = new google.maps.LatLngBounds(
-            new google.maps.LatLng(62.281819, -150.287132),
-            new google.maps.LatLng(62.400471, -150.005608));
-
-
-        // The photograph is courtesy of the U.S. Geological Survey.
-        var srcImage = 'https://developers.google.com/maps/documentation/' +
-            'javascript/examples/full/images/talkeetna.png';
-
-        // The custom USGSOverlay object contains the USGS image,
-        // the bounds of the image, and a reference to the map.
-        overlay = new USGSOverlay(bounds, srcImage, map);
-
-
-        /!** @constructor *!/
-        function USGSOverlay(bounds, image, map) {
-
-            // Initialize all properties.
-            this.bounds_ = bounds;
-            this.image_ = image;
-            this.map_ = map;
-
-            // Define a property to hold the image's div. We'll
-            // actually create this div upon receipt of the onAdd()
-            // method so we'll leave it null for now.
-            this.div_ = null;
-
-            // Explicitly call setMap on this overlay.
-            this.setMap($scope.map);
-        }
-
-
-
-        /!**
-         * onAdd is called when the map's panes are ready and the overlay has been
-         * added to the map.
-         *!/
-        USGSOverlay.prototype.onAdd = function() {
-
-            var div = document.createElement('div');
-            div.style.borderStyle = 'none';
-            div.style.borderWidth = '0px';
-            div.style.position = 'absolute';
-
-            // Create the img element and attach it to the div.
-            var img = document.createElement('img');
-            img.src = this.image_;
-            img.style.width = '100%';
-            img.style.height = '100%';
-            img.style.position = 'absolute';
-            div.appendChild(img);
-
-            this.div_ = div;
-
-            // Add the element to the "overlayLayer" pane.
-            var panes = this.getPanes();
-            panes.overlayLayer.appendChild(div);
-        };
-
-        USGSOverlay.prototype.draw = function() {
-
-            // We use the south-west and north-east
-            // coordinates of the overlay to peg it to the correct position and size.
-            // To do this, we need to retrieve the projection from the overlay.
-            var overlayProjection = this.getProjection();
-
-            // Retrieve the south-west and north-east coordinates of this overlay
-            // in LatLngs and convert them to pixel coordinates.
-            // We'll use these coordinates to resize the div.
-            var sw = overlayProjection.fromLatLngToDivPixel(this.bounds_.getSouthWest());
-            var ne = overlayProjection.fromLatLngToDivPixel(this.bounds_.getNorthEast());
-
-            // Resize the image's div to fit the indicated dimensions.
-            var div = this.div_;
-            div.style.left = sw.x + 'px';
-            div.style.top = ne.y + 'px';
-            div.style.width = (ne.x - sw.x) + 'px';
-            div.style.height = (sw.y - ne.y) + 'px';
-        };
-
-
-*/
-
-
-
-////////// Done
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        // socket to update client directly for new GPS / tips
 
         var socket = io.connect('http://localhost:8080');
         socket.on('GpsPoint', function (data) {
@@ -144,7 +65,7 @@ trackerApp.controller('view2Ctrl', function ($scope, $firebaseObject, $http, $do
                 }
             } else {
 
-                if(data.message != "") { //if message is empty then no need to add and show
+                if (data.message != "") { //if message is empty then no need to add and show
                     $scope.tips.unshift(data);
                     $scope.$apply(); //when we use non angular like JQuery then I need to use this function to update view after pushing data to array scope
                 }
@@ -152,13 +73,15 @@ trackerApp.controller('view2Ctrl', function ($scope, $firebaseObject, $http, $do
                 //zoom to the new GPS point
 
                 //gMap = new google.maps.Map(document.getElementById('map'));
-                $scope.map.setZoom(11);      // This will trigger a zoom_changed on the map
+                $scope.map.setZoom(15);      // This will trigger a zoom_changed on the map
                 $scope.map.setCenter(new google.maps.LatLng(data.latitude, data.longitude));
+                //$scope.map.center =  {lat: data.latitude, lng: data.longitude};
                 //gMap.setMapTypeId(google.maps.MapTypeId.ROADMAP);
 
 
                 //add the GPS point to trqckPath to draw the line in map
                 trackCoordinates.push({lat: JSON.parse(data.latitude), lng: JSON.parse(data.longitude)});
+
 
                 // Define a symbol using SVG path notation, with an opacity of 1.
                 //dashed line
@@ -180,6 +103,8 @@ trackerApp.controller('view2Ctrl', function ($scope, $firebaseObject, $http, $do
                         repeat: '20px'
                     }]
                 });
+
+
                 console.log(trackCoordinates);
                 //each new gps point means that the user is Active
                 var userStatus = document.getElementById(data.email);
@@ -200,24 +125,98 @@ trackerApp.controller('view2Ctrl', function ($scope, $firebaseObject, $http, $do
                         email: results.data.rows[i].email
                     });
                     $scope.id++;
+
+                    users_hash[results.data.rows[i].email] = []; // add users to hash table( it will be used to add all GPS points to the right user)
                 }
             }
-            loadUsers();
+            loadUsers(); //load users to the list in the right side
+
+            //get all GPS points from FireBase and push to the right user in the hashtable
+
+
+            ref.once("value", function (snapshot) {
+                // The callback function will get called twice, once for "fred" and once for "barney"
+                snapshot.forEach(function (childSnapshot) {
+                    // key will be "fred" the first time and "barney" the second time
+                    var key = childSnapshot.key();
+                    // childData will be the actual contents of the child
+                    var childData = childSnapshot.val();
+
+                    if (!childData.hasOwnProperty('active')) { //if Object include active then it means it's not a GPS point with message
+                        users_hash[childData.email].push(childData);
+                    }
+                });
+
+                //build path for each user
+                //loop hashtable
+                var color_index = -1;
+                for (key_name in users_hash) {
+                    console.log(key_name);
+
+                    var path = []; // new path for each user in hashtable
+                    var colors = ['#0000FF', '#D2691E', '#FF0000', '#DAA520']
+
+                    if(colors.length > color_index) // point index for next color only when the index is less than the length (else we will stuck with the same color :) )
+                        color_index++;
+
+
+                    for (var i = 0; i < users_hash[key_name].length; i++) {
+
+                        path.push({
+                            lat: JSON.parse(users_hash[key_name][i].latitude),
+                            lng: JSON.parse(users_hash[key_name][i].longitude)
+                        });
+                        //trackCoordinates.push({lat: JSON.parse(data.latitude), lng: JSON.parse(data.longitude)});
+                    }
+                    // trackPath['path'].push(path);
+
+// Define a symbol using SVG path notation, with an opacity of 1.
+                    //dashed line
+                    var lineSymbol = {
+                        path: 'M 0,-1 0,1',
+                        strokeOpacity: 1,
+                        scale: 4
+                    };
+
+                    var trackPath_users = new google.maps.Polyline({
+                        path: path,
+                        geodesic: true,
+                        strokeColor: colors[color_index],
+                        strokeOpacity: 0,
+                        strokeWeight: 2,
+                        icons: [{
+                            icon: lineSymbol,
+                            offset: '0',
+                            repeat: '20px'
+                        }]
+                    });
+
+                    trackPath_users.setMap($scope.map);
+
+                }
+
+                // var childData.email
+                //    .push({lat: JSON.parse(data.latitude), lng: JSON.parse(data.longitude)});
+
+
+            });
+
+
         })
 
         //load tips (messages from Firebase)
         // Attach an asynchronous callback to read the data at our posts reference
-        ref.once("value", function(snapshot) {
+        ref.once("value", function (snapshot) {
             // The callback function will get called twice, once for "fred" and once for "barney"
-            snapshot.forEach(function(childSnapshot) {
+            snapshot.forEach(function (childSnapshot) {
                 // key will be "fred" the first time and "barney" the second time
                 var key = childSnapshot.key();
                 // childData will be the actual contents of the child
                 var childData = childSnapshot.val();
 
-                if(!childData.hasOwnProperty('active')) { //if Object include active then it means it's not a GPS point with message
-                   // console.log(childData);
-                    if(childData.message != "") { //if message is empty then no need ro add and show
+                if (!childData.hasOwnProperty('active')) { //if Object include active then it means it's not a GPS point with message
+                    // console.log(childData);
+                    if (childData.message != "") { //if message is empty then no need ro add and show
                         $scope.tips.unshift(childData);
                         $scope.$apply(); //when we use non angular like JQuery then I need to use this function to update view after pushing data to array scope
                     }
