226fb360958a48f115e661db96db82b4dc9823d5
Karim Fanadka <kareem9k@gmail.com>
2016-12-24 14:11:36 +0200

    Revert "Delete database.js"
    This reverts commit cb1ad8b66c045459960a7702dbd629da69e51ee8.

---
+++ config/database.js
@@ -0,0 +1,6 @@
+module.exports = {
+	// mongo database connection url
+	url : 'mongodb://localhost/express_angular_mongo_demo',
+	production : 'pg://postgres:1234@localhost:5432/tracker',
+	test: 'pg://karim:1234@localhost:5432/karim'
+};
