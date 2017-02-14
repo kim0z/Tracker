d011734352aa2b40b3aa07aa9b1c401532bbd90f
Karim Fanadka <kareem9k@gmail.com>
2017-02-14 20:48:14 +0200

    get photo GPS

--- public/services/serverSvc.js
+++ public/services/serverSvc.js
@@ -2,17 +2,23 @@
  * Created by karim on 26/09/2016.
  */
 
-trackerApp.service('serverSvc', ['$http', function ($http) {
+trackerApp.service('serverSvc', ['$http', function($http) {
 
-    this.getProviderToken = function (profile) {
+    this.getProviderToken = function(profile) {
         console.log('Client::dataBaseService:: auth0 - get probider token');
         return $http.post('/getProviderToken', profile);
     }
 
+    this.getPhotoMetadata = function(imgUrl) {
+        console.log('Client::get metadata EXIF from photo');
+        return $http.post('/getPhotoMetadata', {img: imgUrl});
+        /*
+        return $http({
+            url: '/getPhotoMetadata',
+            method: "GET",
+            params: { img: imgUrl }
+        });
+        */
+    }
 
 }]);
-
-
-
-
-
