79469a09ad2ea1d8acd31a5e57563b696d1d53e8
Karim Fanadka <kareem9k@gmail.com>
2017-03-15 21:46:28 +0200

    maps search

--- public/views/offlinemap/ctrl.js
+++ public/views/offlinemap/ctrl.js
@@ -688,6 +688,66 @@ trackerApp.controller('offlinemapCtrl', function ($rootScope, $scope, $sce, $tim
                     }
                 });
 
+                // Create the search box and link it to the UI element.
+                var input = document.getElementById('pac-input');
+                var searchBox = new google.maps.places.SearchBox(input);
+                $scope.map.controls[google.maps.ControlPosition.TOP_CENTER].push(input);
+
+                // Bias the SearchBox results towards current map's viewport.
+                $scope.map.addListener('bounds_changed', function() {
+                    searchBox.setBounds($scope.map.getBounds());
+                });
+
+                var markers_searh = [];
+                // Listen for the event fired when the user selects a prediction and retrieve
+                // more details for that place.
+                searchBox.addListener('places_changed', function() {
+                    var places = searchBox.getPlaces();
+
+                    if (places.length == 0) {
+                        return;
+                    }
+
+                    // Clear out the old markers.
+                    markers_searh.forEach(function(marker) {
+                        marker.setMap(null);
+                    });
+                    markers_searh = [];
+
+                    // For each place, get the icon, name and location.
+                    var bounds = new google.maps.LatLngBounds();
+                    places.forEach(function(place) {
+                        if (!place.geometry) {
+                            console.log("Returned place contains no geometry");
+                            return;
+                        }
+                        var icon = {
+                            url: place.icon,
+                            size: new google.maps.Size(71, 71),
+                            origin: new google.maps.Point(0, 0),
+                            anchor: new google.maps.Point(17, 34),
+                            scaledSize: new google.maps.Size(25, 25)
+                        };
+
+                        // Create a marker for each place.
+                        markers_searh.push(new google.maps.Marker({
+                            map: $scope.map,
+                            icon: icon,
+                            title: place.name,
+                            position: place.geometry.location
+                        }));
+
+                        if (place.geometry.viewport) {
+                            // Only geocodes have viewport.
+                            bounds.union(place.geometry.viewport);
+                        } else {
+                            bounds.extend(place.geometry.location);
+                        }
+                    });
+                    $scope.map.fitBounds(bounds);
+                });
+
+
                 var drawingManager = new google.maps.drawing.DrawingManager({
                     drawingMode: google.maps.drawing.OverlayType.MARKER,
                     drawingControl: true,
