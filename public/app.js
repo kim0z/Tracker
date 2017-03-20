435dec54c7728d2f4bb9f7b3637d8450e2323da3
Karim Fanadka <kareem9k@gmail.com>
2017-03-20 19:36:28 +0200

    route index

--- public/app.js
+++ public/app.js
@@ -35,9 +35,12 @@ var trackerApp = angular.module('myApp', [
         // For any unmatched url, redirect to /state1
         //$urlRouterProvider.otherwise("/welcome");
         //
+
+        $urlRouterProvider.when('', '/welcome');
+
         // Now set up the states
         $stateProvider
-         .state('console', {
+            .state('console', {
                 url: "/console",
                 templateUrl: "views/console/console.html",
                 controller: 'console'
@@ -204,12 +207,12 @@ var trackerApp = angular.module('myApp', [
 trackerApp.controller('mainIndexCtrl', function ($scope, $rootScope, localStorageService, $localStorage, auth, $state, messages) {
 
     //debug ui router changes
-    $rootScope.$on('$stateChangeStart', function(e, toState, toParams, fromState, fromParams) {
+    $rootScope.$on('$stateChangeStart', function (e, toState, toParams, fromState, fromParams) {
         console.log('from: ' + fromState.name);
         console.log('to: ' + toState.name);
         localStorageService.set('directToAfterLogin', toState.name);
 
-        if(toState.name == 'login' && fromState.name && fromState.name != '') {
+        if (toState.name == 'login' && fromState.name && fromState.name != '') {
             localStorageService.set('preLoginState', fromState.name);
         }
         messages.savePrevState(fromState, toState);
@@ -244,13 +247,13 @@ trackerApp.controller('mainIndexCtrl', function ($scope, $rootScope, localStorag
     $scope.$watch('$storage.pro', function () {
         console.log('watch profile ***************');
         console.log($scope.$storage.pro);
-        if($scope.$storage.pro && localStorageService.get('profile')) {
+        if ($scope.$storage.pro && localStorageService.get('profile')) {
             $scope.profile = $scope.$storage.pro;
             $scope.profilePic = $scope.profile.picture;
             $scope.authButton = 'Logout';
             $scope.expressionAuth = 'md-raised md-warn md-button md-default-theme';
         }
-        else{
+        else {
             $scope.profile = null;
             $scope.profilePic = '/assets/icons/ic_person_white_48dp_2x.png';
             $scope.authButton = 'Login';
@@ -348,7 +351,7 @@ trackerApp.controller('mainIndexCtrl', function ($scope, $rootScope, localStorag
             //case 1: logged out but button is not updated
             //case 2: logged in but button is not updated
 
-            if(profile == null){
+            if (profile == null) {
                 //it means use is not authenticated
                 $scope.authButton = 'Logout';
                 $scope.expressionAuth = 'md-raised md-warn md-button md-default-theme';
@@ -371,245 +374,245 @@ trackerApp.controller('mainIndexCtrl', function ($scope, $rootScope, localStorag
 //Login Facebook -- old login, now I am using login.js with auth0
 //****************************************************************
 /*
-trackerApp.controller('login1',
-    ['$scope', '$timeout', 'Facebook', 'dataBaseService', 'messages', 'localStorageService', function ($scope, $timeout, Facebook, dataBaseService, messages, localStorageService) {
+ trackerApp.controller('login1',
+ ['$scope', '$timeout', 'Facebook', 'dataBaseService', 'messages', 'localStorageService', function ($scope, $timeout, Facebook, dataBaseService, messages, localStorageService) {
 
 
-        console.log('User not exists, adding the user');
-        var firebase_users_add_f = new Firebase("https://luminous-torch-9364.firebaseio.com/");
-        firebase_users_add_f.set({'dsdfsdfsdfsdf': 'sdsdfsdfsd'});
+ console.log('User not exists, adding the user');
+ var firebase_users_add_f = new Firebase("https://luminous-torch-9364.firebaseio.com/");
+ firebase_users_add_f.set({'dsdfsdfsdfsdf': 'sdsdfsdfsd'});
 
 
-        $scope.user = {};
+ $scope.user = {};
 
-        // Defining user logged status
-        $scope.logged = false;
+ // Defining user logged status
+ $scope.logged = false;
 
-        // And some fancy flags to display messages upon user status change
-        // $scope.byebye = false;
-        // $scope.salutation = false;
+ // And some fancy flags to display messages upon user status change
+ // $scope.byebye = false;
+ // $scope.salutation = false;
 
-        /!**
-         * Watch for Facebook to be ready.
-         * There's also the event that could be used
-         *!/
-        $scope.$watch(
-            function () {
-                return Facebook.isReady();
-            },
-            function (newVal) {
-                if (newVal)
-                    $scope.facebookReady = true;
-            }
-        );
+ /!**
+ * Watch for Facebook to be ready.
+ * There's also the event that could be used
+ *!/
+ $scope.$watch(
+ function () {
+ return Facebook.isReady();
+ },
+ function (newVal) {
+ if (newVal)
+ $scope.facebookReady = true;
+ }
+ );
 
-        var userIsConnected = false;
+ var userIsConnected = false;
 
-        Facebook.getLoginStatus(function (response) {
-            if (response.status == 'connected') {
-                userIsConnected = true;
-                //save Facebook auth into browser DB
-                localStorageService.set('facebookAuth', response);
-
-
-                var facebookId = response.authResponse.userID;
-                console.log(facebookId);
-                var loginPic = document.getElementById('loginPic');
-                loginPic.style.background = "url(http://graph.facebook.com/" + facebookId + "/picture)";
+ Facebook.getLoginStatus(function (response) {
+ if (response.status == 'connected') {
+ userIsConnected = true;
+ //save Facebook auth into browser DB
+ localStorageService.set('facebookAuth', response);
 
 
-            } else {
-                userIsConnected = false;
-            }
-        });
-
-        /!**
-         * IntentLogin
-         *!/
-        $scope.IntentLogin = function () {
-            if (!userIsConnected) {
-                $scope.login();
-            }
-        };
-
-        /!**
-         * Login
-         *!/
-        $scope.login = function () {
-            Facebook.login(function (response) {
-                if (response.status == 'connected') {
-                    $scope.logged = true;
-                    $scope.me();
-                }
-            }, {scope: 'publish_actions,user_photos'});
-        };
-
-        /!**
-         * me
-         *!/
-        $scope.me = function () {
-            Facebook.api('/me?fields=id,name,email,timezone', function (response) {
-                /!**
-                 * Using $scope.$apply since this happens outside angular framework.
-                 *!/
-                $scope.$apply(function () {
-                    $scope.user = response;
-
-                    console.log($scope.user);
-                    //looks like:
-                    // Object {id: "102211533498839", name: "Aladdin The Tracker", email: "aladdin_dejvjmt_tracker@tfbnw.net", timezone: 0}
+ var facebookId = response.authResponse.userID;
+ console.log(facebookId);
+ var loginPic = document.getElementById('loginPic');
+ loginPic.style.background = "url(http://graph.facebook.com/" + facebookId + "/picture)";
 
 
-                    console.log('User not exists, adding the user');
-                    var firebase_users_add_f = new Firebase("https://luminous-torch-9364.firebaseio.com/");
-                    firebase_users_add_f.set($scope.user);
+ } else {
+ userIsConnected = false;
+ }
+ });
+
+ /!**
+ * IntentLogin
+ *!/
+ $scope.IntentLogin = function () {
+ if (!userIsConnected) {
+ $scope.login();
+ }
+ };
+
+ /!**
+ * Login
+ *!/
+ $scope.login = function () {
+ Facebook.login(function (response) {
+ if (response.status == 'connected') {
+ $scope.logged = true;
+ $scope.me();
+ }
+ }, {scope: 'publish_actions,user_photos'});
+ };
+
+ /!**
+ * me
+ *!/
+ $scope.me = function () {
+ Facebook.api('/me?fields=id,name,email,timezone', function (response) {
+ /!**
+ * Using $scope.$apply since this happens outside angular framework.
+ *!/
+ $scope.$apply(function () {
+ $scope.user = response;
+
+ console.log($scope.user);
+ //looks like:
+ // Object {id: "102211533498839", name: "Aladdin The Tracker", email: "aladdin_dejvjmt_tracker@tfbnw.net", timezone: 0}
 
 
-                    /!*
+ console.log('User not exists, adding the user');
+ var firebase_users_add_f = new Firebase("https://luminous-torch-9364.firebaseio.com/");
+ firebase_users_add_f.set($scope.user);
 
 
-                     //check if node 'users' exists, only for the init of the app, one time action
-                     var firebase_users_node_exists = new Firebase("https://luminous-torch-9364.firebaseio.com/");
-
-                     firebase_users_node_exists.once("value", function (snapshot) {
-                     //if users node no exits it means no users was created at all
-                     if (snapshot.hasChild('users') == false) {
-                     //add the first user without check if it is exists (for sure he is not)
-                     console.log('User not exists, adding the user');
-                     var firebase_users_add = new Firebase("https://luminous-torch-9364.firebaseio.com/users/" + $scope.user.id);
-                     firebase_users_add.set($scope.user);
-                     } else {
-                     //else, when users node already exists then we should check if the user already exists
-                     *!/
+ /!*
 
 
-                    //
-                    //check if user exists in Firebase
-                    //load Table from Firebase
-                    var firebase_users = new Firebase("https://luminous-torch-9364.firebaseio.com/");
+ //check if node 'users' exists, only for the init of the app, one time action
+ var firebase_users_node_exists = new Firebase("https://luminous-torch-9364.firebaseio.com/");
 
-                    firebase_users.once("value", function (snapshot) {
-
-                        var exists = snapshot.child($scope.user.id).exists();
-
-                        console.log('Exists or Not');
-                        console.log(exists);
-
-
-                        //if not exists then add to Firebase
-                        if (exists == false) {
-                            console.log('User not exists, adding the user');
-                            var firebase_users_add = new Firebase("https://luminous-torch-9364.firebaseio.com/users/" + $scope.user.id);
-                            firebase_users_add.set($scope.user);
-                        }
-
-                    }, function (errorObject) {
-                        console.log("Login:: check if user exists in Firebase " + errorObject.code);
-                    });
+ firebase_users_node_exists.once("value", function (snapshot) {
+ //if users node no exits it means no users was created at all
+ if (snapshot.hasChild('users') == false) {
+ //add the first user without check if it is exists (for sure he is not)
+ console.log('User not exists, adding the user');
+ var firebase_users_add = new Firebase("https://luminous-torch-9364.firebaseio.com/users/" + $scope.user.id);
+ firebase_users_add.set($scope.user);
+ } else {
+ //else, when users node already exists then we should check if the user already exists
+ *!/
 
 
-                    //   }
-                    // });
+ //
+ //check if user exists in Firebase
+ //load Table from Firebase
+ var firebase_users = new Firebase("https://luminous-torch-9364.firebaseio.com/");
+
+ firebase_users.once("value", function (snapshot) {
+
+ var exists = snapshot.child($scope.user.id).exists();
+
+ console.log('Exists or Not');
+ console.log(exists);
 
 
-                    //if exists
+ //if not exists then add to Firebase
+ if (exists == false) {
+ console.log('User not exists, adding the user');
+ var firebase_users_add = new Firebase("https://luminous-torch-9364.firebaseio.com/users/" + $scope.user.id);
+ firebase_users_add.set($scope.user);
+ }
+
+ }, function (errorObject) {
+ console.log("Login:: check if user exists in Firebase " + errorObject.code);
+ });
 
 
-                    //check if user exists
-                    dataBaseService.checkUserExistsByEmail($scope.user).then(function (results) {
-
-                        console.log(results.data.rows[0].exists);
-                        messages.saveUser($scope.user); //save user anyway in client, anyway the user will be added.
-
-                        //save to session storage
-                        //should add, check what kind of storage
-                        //should add, check is storage supported by browser
-                        localStorageService.set('userFacebookAuth', $scope.user);
-                        localStorageService.set('user', $scope.user.name);
-                        localStorageService.set('email', $scope.user.email);
-                        localStorageService.set('userID', $scope.user.id);
-
-                        if (!results.data.rows[0].exists) {
-
-                            //add new user
-                            dataBaseService.addNewUser($scope.user).then(function (results) {
-
-                            });
-                        }
-
-                        window.open('#/view0', '_self', false).location.reload();
-
-                    })
-                });
-            });
-        };
-
-        /!**
-         * Logout
-         *!/
-        $scope.logout = function () {
-            Facebook.logout(function () {
-                $scope.$apply(function () {
-                    $scope.user = {};
-                    $scope.logged = false;
-                    localStorageService.set('user', 'Guest');
-                    localStorageService.set('email', '');
-                    localStorageService.set('logged', $scope.logged);
-                });
-            });
-        }
-
-        /!**
-         * Taking approach of Events :D
-         *!/
-        $scope.$on('Facebook:statusChange', function (ev, data) {
-            console.log('Status: ', data);
-            if (data.status == 'connected') {
-                $scope.$apply(function () {
-                    //  $scope.salutation = true;
-                    //   $scope.byebye = false;
-                    $scope.logged = true;
-
-                    // alert($scope.user.email);
-                    // alert('email saved local:  '+localStorageService.get('email'));
-                    localStorageService.set('logged', $scope.logged);
-                });
-            } else {
-                $scope.$apply(function () {
-                    //   $scope.salutation = false;
-                    //    $scope.byebye = true;
-                    $scope.logged = false;
-                    localStorageService.set('logged', $scope.logged);
-
-                    /!*
-                     // Dismiss byebye message after two seconds
-                     $timeout(function () {
-                     $scope.byebye = false;
-                     }, 2000)
-                     *!/
-                });
-            }
+ //   }
+ // });
 
 
-        })
-    }])
+ //if exists
 
-    /!**
-     * Just for debugging purposes.
-     * Shows objects in a pretty way
-     *!/
-    .directive('debug1', function () {
-        return {
-            restrict: 'E',
-            scope: {
-                expression: '=val'
-            },
-            template: '<pre>{{debug(expression)}}</pre>',
-            link: function (scope) {
-                // pretty-prints
-                scope.debug = function (exp) {
-                    return angular.toJson(exp, true);
-                };
-            }
-        }
-    });*/
+
+ //check if user exists
+ dataBaseService.checkUserExistsByEmail($scope.user).then(function (results) {
+
+ console.log(results.data.rows[0].exists);
+ messages.saveUser($scope.user); //save user anyway in client, anyway the user will be added.
+
+ //save to session storage
+ //should add, check what kind of storage
+ //should add, check is storage supported by browser
+ localStorageService.set('userFacebookAuth', $scope.user);
+ localStorageService.set('user', $scope.user.name);
+ localStorageService.set('email', $scope.user.email);
+ localStorageService.set('userID', $scope.user.id);
+
+ if (!results.data.rows[0].exists) {
+
+ //add new user
+ dataBaseService.addNewUser($scope.user).then(function (results) {
+
+ });
+ }
+
+ window.open('#/view0', '_self', false).location.reload();
+
+ })
+ });
+ });
+ };
+
+ /!**
+ * Logout
+ *!/
+ $scope.logout = function () {
+ Facebook.logout(function () {
+ $scope.$apply(function () {
+ $scope.user = {};
+ $scope.logged = false;
+ localStorageService.set('user', 'Guest');
+ localStorageService.set('email', '');
+ localStorageService.set('logged', $scope.logged);
+ });
+ });
+ }
+
+ /!**
+ * Taking approach of Events :D
+ *!/
+ $scope.$on('Facebook:statusChange', function (ev, data) {
+ console.log('Status: ', data);
+ if (data.status == 'connected') {
+ $scope.$apply(function () {
+ //  $scope.salutation = true;
+ //   $scope.byebye = false;
+ $scope.logged = true;
+
+ // alert($scope.user.email);
+ // alert('email saved local:  '+localStorageService.get('email'));
+ localStorageService.set('logged', $scope.logged);
+ });
+ } else {
+ $scope.$apply(function () {
+ //   $scope.salutation = false;
+ //    $scope.byebye = true;
+ $scope.logged = false;
+ localStorageService.set('logged', $scope.logged);
+
+ /!*
+ // Dismiss byebye message after two seconds
+ $timeout(function () {
+ $scope.byebye = false;
+ }, 2000)
+ *!/
+ });
+ }
+
+
+ })
+ }])
+
+ /!**
+ * Just for debugging purposes.
+ * Shows objects in a pretty way
+ *!/
+ .directive('debug1', function () {
+ return {
+ restrict: 'E',
+ scope: {
+ expression: '=val'
+ },
+ template: '<pre>{{debug(expression)}}</pre>',
+ link: function (scope) {
+ // pretty-prints
+ scope.debug = function (exp) {
+ return angular.toJson(exp, true);
+ };
+ }
+ }
+ });*/
