a60ec3d9108397ad0a8cfb5b3ed0de9202a037bb
Karim Fanadka <kareem9k@gmail.com>
2017-05-28 20:44:22 +0300

    ui fix

--- public/views/trip/tripCtrl.js
+++ public/views/trip/tripCtrl.js
@@ -959,7 +959,7 @@ trackerApp.controller('tripCtrl', function ($rootScope, $scope, $sce, $q, $timeo
                     drawingMode: google.maps.drawing.OverlayType.MARKER,
                     drawingControl: true,
                     drawingControlOptions: {
-                        position: google.maps.ControlPosition.RIGHT_TOP,
+                        position: google.maps.ControlPosition.TOP_CENTER,
                         drawingModes: ['marker', 'circle', 'polygon', 'polyline', 'rectangle']
                     },
                     markerOptions: {icon: 'https://developers.google.com/maps/documentation/javascript/examples/full/images/beachflag.png'},
