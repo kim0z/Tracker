72814258c12d2d825307a1f672495a4c4f46bca6
Karim Fanadka <kareem9k@gmail.com>
2017-06-01 02:08:52 +0300

    fix

--- config/database.js
+++ config/database.js
@@ -3,5 +3,4 @@ module.exports = {
 	production : 'pg://postgres:1234@localhost:5432/tracker',
 	test: 'pg://karim:1234@localhost:5432/karim',
 	test_work: 'pg://postgres:1234@localhost:5432/karim'
-
 };
