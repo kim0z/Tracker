cbfdc73c68f56e53628f5799f0339142786c7dad
Karim Fanadka <kareem9k@gmail.com>
2017-09-28 18:38:20 +0300

    again

--- public/views/console/console.js
+++ public/views/console/console.js
@@ -8,9 +8,9 @@ trackerApp.controller('console', function ($scope, messages) {
     }
 
     for(var i = 0 ; i < $scope.path_not_hash.length ; i++){
-        if($scope.path_not_hash[i].lat > $scope.path_not_hash[i+1].lat || $scope.path_not_hash[i].lng > $scope.path_not_hash[i+1].lng){
-            console.error($scope.path_not_hash[i].lat);
-            console.error($scope.path_not_hash[i].lng);
+        if($scope.path_not_hash[i].timestamp > $scope.path_not_hash[i+1].timestamp) {
+            console.error($scope.path_not_hash[i].timestamp);
+            console.error($scope.path_not_hash[i+1].timestamp);
             console.log('..........................');
         }
     }
