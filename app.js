541f03fa13b4561de5746584098190cedd290603
Karim Fanadka <kareem9k@gmail.com>
2017-10-01 00:57:52 +0300

    again :(

--- app.js
+++ app.js
@@ -288,16 +288,17 @@ app.post('/getTripPath', function (request, response) {
         });
 
         for (var i = 0; i < trip_path.length; i++) {
-            console.log(trip_path[i]);
-            if (!trip_path[i].hasOwnProperty('timestamp')) {
-                console.log('Has no timestamp:::::::')
-                console.log(trip_path[i]);
-                console.log('******************************')
-            } else if (trip_path[i].timestamp > trip_path[i + 1].timestamp) {
-                console.log('index : ' + i)
-                console.log(trip_path[i].timestamp);
-                console.log(trip_path[i + 1].timestamp);
-                console.log('..........................');
+            if(trip_path[i] != null){
+                if (!trip_path[i].hasOwnProperty('timestamp')) {
+                    console.log('Has no timestamp:::::::')
+                    console.log(trip_path[i]);
+                    console.log('******************************')
+                } else if (trip_path[i].timestamp > trip_path[i + 1].timestamp) {
+                    console.log('index : ' + i)
+                    console.log(trip_path[i].timestamp);
+                    console.log(trip_path[i + 1].timestamp);
+                    console.log('..........................');
+                }
             }
         }
 
