6075539759df6419e4ef4a35da5d7aebfbd1375c
Karim Fanadka <kareem9k@gmail.com>
2018-07-25 18:59:24 +0300

    get path from postgres

--- app.js
+++ app.js
@@ -373,6 +373,104 @@ app.post('/getTripPath', function (request, response) {
         console.log("The read failed: " + errorObject.code);
     });
 });
+
+//Get Path from Firebase
+app.post('/getTripPathPostgres', function (request, response) {
+    console.log('SERVER:: Posgtres::  Get Trip Path');
+    var tripDays = request.body.tripDays;
+    console.log('Trip days: ' + request.body.tripDays);
+    console.log('User ID: ' + request.body.userId);
+    console.log('Trip Id: ' + request.body.tripId);
+
+   // firebase_trip_path.once("value", function (snapshot) {
+
+
+        getPathJsonPostgres(request.body.tripId).then(function (results) {
+
+            //Trying to get Path from Postgres
+            console.log(results)
+            var trip_path = results;
+
+            console.log('Trip path loaded');
+            console.log('Trip path length : ' + trip_path.length);
+
+        //When path loaded, sort it by timestamp - the path could be sorted by default ????????????
+        //trip_path.sort(function (a, b) {
+        //    return new Date(a.timestamp) - new Date(b.timestamp);
+        //});
+        //when path sorted, save it into hash table for easy use
+        //$scope.trip_path_hash [0] = $scope.trip_path; //day zero is all the trip
+        var path_first_date = '';
+        for (var trip_first_day_index = 0; trip_first_day_index < trip_path.length; trip_first_day_index++) {
+            if (trip_path[trip_first_day_index].timestamp != null) {
+                console.log('First date found in the GPS points array:: '+ trip_path[trip_first_day_index].timestamp);
+                path_first_date = trip_path[trip_first_day_index].timestamp; //know what is the first datei
+                path_first_date = new Date(parseInt(path_first_date));
+                path_first_date = path_first_date.toISOString();
+                break;
+            }
+        }
+        console.log('first date: ' + path_first_date);
+
+        var day = 0;
+        var path_last_index = 0;
+        var trip_path_hash = [];
+        //$scope.trip_path_hash = new Array($scope.tripDays + 1);
+        for (var hash_index = 0; hash_index < tripDays + 1; hash_index++) { //init hashtable with extra 10 cells, I removed the 10 extra no need
+            trip_path_hash[hash_index] = [];
+        }
+
+        for (var i = 0; i < tripDays - 1; i++) {
+
+            for (var j = path_last_index; j < trip_path.length; j++) { //each day should be saved into new celi
+
+                if(day < tripDays){
+                    console.log('Path Index:: ' + j + ' of ' + trip_path.length)
+                    console.log('GPS Point date:: ' + trip_path[j].timestamp);
+                    var d = new Date(parseInt(trip_path[j].timestamp));
+                    trip_path[j].timestamp = d;
+                    trip_path[j].timestamp = trip_path[j].timestamp.toISOString();
+                    if (trip_path[j]['timestamp'] && path_first_date) {
+                        console.log('GPS Point date after convert '+trip_path[j].timestamp.substring(0, 10));
+                        console.log('First date in loop:: ' + path_first_date);
+
+                        //trip_path[j].timestamp = trip_path[j].timestamp.toString();
+                        //path_first_date = path_first_date.toString();
+
+                        if (trip_path[j].timestamp.substring(0, 10) == path_first_date.substring(0, 10)) {
+                            if (checkAccuracy(trip_path[j], gps_accuracy)) { //check accuracy
+                                trip_path_hash[day].push(
+                                    {
+                                        lat: JSON.parse(trip_path[j]['coords'].latitude),
+                                        lng: JSON.parse(trip_path[j]['coords'].longitude),
+                                        timestamp: trip_path[j]['timestamp'],
+                                        data: trip_path[j]
+                                    }
+                                );
+                            }
+                        } else {
+                            //if date changed it means new day started, updated day and path index
+                            console.log('day');
+                            console.log('Starting new date ' + day++);
+                            day++;
+                            path_last_index = j;
+                            path_first_date = trip_path[j].timestamp;
+                            console.log('new first date: '+path_first_date);
+
+                        }
+                    }
+                } else {
+                    console.log('Trip path not sliced into hash because of date issue')
+                }
+            }
+        }
+        //console.log('HASH');
+        //console.log(trip_path_hash);
+        response.send(trip_path_hash);
+    }, function (errorObject) {
+        console.log("Path read from Postgres failed: ");
+    });
+});
 /*app.post('/getTripPath', function (request, response) {
     console.log('SERVER:: Firebase::  Get Trip Path');
     var tripDays = request.body.tripDays;
@@ -815,22 +913,22 @@ app.post('/publicTrip', function (request, response) {
 });
 
 //get path
-app.post('/getPathJsonPostgres', function (request, response) {
-
+//app.post('/getPathJsonPostgres', function (request, response) {
+var getPathJsonPostgres = function(tripid) {
     console.log('SERVER:: Postgres:: get Trip path from postgres');
     var results = [];
-    console.log(request.body);
+    //console.log(request.body);
     // Get a Postgres client from the connection pool
     pg.connect(conString, function (err, client, done) {
         // Handle connection errors
         if (err) {
             done();
             console.log(err);
-            return response.status(500).json({success: false, data: err});
+            //return response.status(500).json({success: false, data: err});
         }
         //var email = "'" + request.body.email + "'";
         // SQL Query > Select Data
-        var query = client.query("SELECT path FROM trips WHERE id = ($1)",[request.body.id]);
+        var query = client.query("SELECT path FROM trips WHERE id = ($1)", [tripid]); //request.body.id
 
         console.log(query);
         // Stream results back one row at a time
@@ -845,7 +943,8 @@ app.post('/getPathJsonPostgres', function (request, response) {
             return response.json(results);
         });
     });
-});
+}
+//});
 //save path
 app.post('/savePathJsonPostgres', function (request, response) {
     console.log('SERVER:: Postgres:: save / update trip with path' + request.body);
